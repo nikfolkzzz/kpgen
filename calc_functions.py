14b4e39f1af8e3af0bfb35fc68f00a11801f3417
Andru Nikolaev <aeanst514@yandex.ru>
2021-08-08 15:19:26 +0400

    add docx

--- calc_functions.py
+++ calc_functions.py
@@ -6,87 +6,10 @@ from math import floor
 
 
 
-def rect_flange(arr,name): 
-    amin, bmin, thick , amax , bmax = arr
-    mass = (amax*bmax - amin*bmin)*thick*7850*10**-9
-    mass = round(mass,1)
-    cost = mass*700*2
-    cost =round(int(cost),1)
-    answer = f'''
-    {name}
-    масса одного фланца {mass}
-    масса двух фланцев {mass*2}
-    цена за два фланца {cost} ₽
-    '''   
-
-    answer = {
-        'cost':cost,
-        'answer':answer,
-    }
-
-    return answer
-
-
-def rect_fej(arr, name): 
-    A, B , L , C = arr 
-    perimetr = (A+B)*2
-    full_len  = L + 2*C
-    area = round(perimetr*full_len*10**-6 , 1)
-    cost = area * 25000 
-    answer = f'''
-    тканевый: {name} 
-    площадь компенсатора тип 2: {area}
-    цена: {cost}
-        '''
-
-    answer = {
-        'cost':cost,
-        'answer':answer,
-    }
-
-
-
-    return answer
-
-
-def rect_cflange(arr,name): 
-    amin, bmin, thick , amax , bmax = arr
-    mass = (amax*bmax - amin*bmin)*thick*7850*10**-9
-    cost = mass*700*2
-    answer = f'''
-    {name}
-    масса одной планки {mass}
-    масса двух планок {mass*2}
-    цена за две планки {cost} ₽
-    '''   
-
-    answer = {
-        'cost':cost,
-        'answer':answer,
-    }
-
-    return answer
-
-def bolting_calc(arr,name):
-    qty = arr[0]
-
-    cost = qty *70 
-    answer =f'''
-    количество крепежа: {qty}
-    цена крепежа: {cost}
-    
-    '''
-
-    answer = {
-        'cost':cost,
-        'answer':answer,
-    }
-
-    return answer
 
 
 def circle_fej(arr, name , mp , bp ,fp ):
-    fabric_price = int(fp)
+    fabric_price = floor(int(fp))
     metall_price = mp
     bolt_price = bp
 
@@ -100,10 +23,10 @@ def circle_fej(arr, name , mp , bp ,fp ):
         'cost': price,
         'answer': f'''
 
-    цена тканевой части за кв м {fabric_price}
-    name : {name}
-    площадь компенсатора - {area}
-    цена  {price}
+цена тканевой части за кв м {fabric_price}
+name : {name}
+площадь компенсатора - {area}
+цена  {price}
     '''
     }
 
